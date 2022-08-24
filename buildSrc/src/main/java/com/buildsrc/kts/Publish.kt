68ed374d2e8593614712ca1276b46de668259d3a
WangNeng <873456775@qq.com>
2022-08-24 15:08:53 +0800

    版本1.4.5

--- buildSrc/src/main/java/com/buildsrc/kts/Publish.kt
+++ buildSrc/src/main/java/com/buildsrc/kts/Publish.kt
@@ -10,8 +10,8 @@ import java.io.File
  *create by zhusw on 5/6/21 16:43
  */
 
-private const val VERSION = "1.0.4"
-private const val SNAPSHOT = true
+private const val VERSION = "1.0.5"
+private const val SNAPSHOT = false
 
 object Publish {
     object Version {
